31bf22246e1763bba8f271d60280f232aa0506e0
sourlodine <harran39318@gmail.com>
2024-08-16 20:13:38 +0900

    fix: add mouse event

--- src/pages/BetsPage/BetsPage.tsx
+++ src/pages/BetsPage/BetsPage.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState, type FC } from "react";
+import { useEffect, useRef, useState, type FC } from "react";
 
 import { shuffleArray } from "@/utils/helpers";
 import { semiFinalists, realFinalists } from "@/utils/constants";
@@ -17,6 +17,8 @@ import { useTonConnect } from "../../hooks/useTonConnect";
 import { useBettingContract } from "@/hooks/useBettingContract";
 import { GameState } from "@/states/GameState";
 import { Dino } from "react-dino-game";
+import DinoGame from "react-chrome-dino-ts";
+import "react-chrome-dino-ts/index.css";
 
 export const BetsPage: FC = () => {
   const [shuffledArray, setShuffledArray] = useState(() =>
@@ -38,13 +40,33 @@ export const BetsPage: FC = () => {
   const { connected } = useTonConnect();
   const { betsCounter } = useBettingContract();
 
+  function handleEvent(event: any) {
+    // Do something in response to the event
+    console.log("Event occurred:", event.type);
+    const spaceEvent = new KeyboardEvent("keydown", {
+      key: " ",
+      code: "Space",
+      keyCode: 32, // Deprecated but may be needed in some environments
+      which: 32, // Deprecated but may also be needed
+      bubbles: true,
+      cancelable: true,
+    });
+
+    // Dispatch the event to the document or a specific element
+    document.dispatchEvent(spaceEvent);
+  }
+
   return (
     <>
       <div className="bg-gray-100 w-full h-screen flex flex-col items-center justify-center">
         <div className="flex flex-row m-4 justify-between">
           <TonConnectButton />
         </div>
-        {connected && <Dino />}
+        {connected && (
+          <div onClick={handleEvent}>
+            <DinoGame />
+          </div>
+        )}
         {/* <FilterButtons onFilter={filterAndShuffle} /> */}
       </div>
       {/* {isInfoPopupVisible && (
